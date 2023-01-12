23fd6fd2754a4556f6b09bf7e441bfb21b28a5e9
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-01-12 11:16:06 -0800

    item model && pokemon type model

--- flask-backend/app/models.py
+++ flask-backend/app/models.py
@@ -2,7 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
-
+"""
+pokemonType relationship ??
+"""
 
 class Pokemon(db.Model): 
   __tablename__ = "pokemons"
@@ -10,12 +12,36 @@ class Pokemon(db.Model):
   number = db.Column(db.Integer, unique=True, nullable=False)
   attack = db.Column(db.Integer, nullable=False)
   defense = db.Column(db.Integer, nullable=False)
-  imageUrl = db.Column(db.String, nullable=False)
+  image_url = db.Column(db.String, nullable=False)
   name = db.Column(db.String(255), unique=True, nullable=False)
   type = db.Column(db.String(255), nullable=False)
   moves = db.Column(db.String(255), nullable=False)
-  encounterRate = db.Column(db.Float)
+  encounter_rate = db.Column(db.Float)
+  catch_rate = db.Column(db.Float)
   captured = db.Column(db.Boolean)
+
+  items = db.relationship('Item',back_populates='pokemon', cascade="all, delete")
+
+class Item(db.Model):
+  __tablename__='items'
+  id = db.Column(db.Integer, primary_key=True)
+  happiness = db.Column(db.Integer)
+  image_url = db.Column(db.String(255), nullable=False)
+  name = db.Column(db.String(255), nullable=False)
+  price = db.Column(db.Integer, nullable=False)
+  pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemons.id'),nullable=False)
+  pokemon = db.relationship("Pokemon", back_populates='items')
+
+
+class PokemonType(db.Model):
+  __tablename__='pokemon_types'
+  id = db.Column(db.Integer, primary_key=True)
+  types = db.Column(db.String(255), nullable=False)
+
+
+
+
+
 
 
 
